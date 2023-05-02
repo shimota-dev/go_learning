516e7dd336744d047a67e8a104b5f58507bdacc5
Wataru Shimota <w.shimota@gmail.com>
2023-05-02 20:55:50 +0900

    added packageone comment

--- scope/packageone/package.go
+++ scope/packageone/package.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 )
 
-var PackageVar = "This is packagevar"
+var PackageVar = "This is packagevar in the packageone"
 
 func PrintMe(s1, s2 string) {
 	fmt.Println(s1, s2, PackageVar)
+
 }
