f68bc8284d19ba56292cc58786a42af21f4cbea3
Wataru Shimota <w.shimota@gmail.com>
2023-05-02 20:57:23 +0900

    added block level comment

--- scope/main.go
+++ scope/main.go
@@ -8,7 +8,7 @@ var myVar = "this is my var in the package level"
 
 func main() {
 
-	var blockVar = "this is blockvar"
+	var blockVar = "this is blockvar in the block level"
 
 	packageone.PrintMe(myVar, blockVar)
 }
